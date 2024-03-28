b12c3d47d0c3c1b839686cb0938467d6a620d736
nq203 <quanchun8@gmail.com>
2024-03-28 11:48:12 +0700

    mm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import Form from "./Form.js";
+import CountryInfo from "./CountryAPI.js";
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [inputError, setInputError] = useState(null);
+  function handleInputChange(event) {
+    const value = event.target.value;
+    setInputValue(value);
+    if (value.length < 5) {
+      setInputError("Input must be at least 5 characters");
+    } else {
+      setInputError(null);
+    }
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (inputValue.length >= 5) {
+      // submit form
+    } else {
+      setInputError("Input must be at least 5 characters");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <form onSubmit={handleSubmit}>
+    //   <label>
+    //     Fruit:
+    //     <input type="text" value={inputValue} onChange={handleInputChange} />
+    //   </label>
+    //   {inputError && <div style={{ color: "red" }}>{inputError}</div>}
+    //   <button type="submit">Submit</button>
+    // </form>
+    <CountryInfo />
+    // <Form />
   );
-}
-
-export default App;
+};
